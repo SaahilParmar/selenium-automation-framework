d81247cae70604cf91d977987cbcabe2443f608c
SAAHIL PARMAR <116298945+SaahilParmar@users.noreply.github.com>
2025-06-28 15:16:01 +0000

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ def driver(request):
     if browser == "chrome":
         options = Options()
         if headless:
-            options.add_argument("--headless")
+            options.add_argument("--headless=new")
             options.add_argument("--disable-gpu")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
